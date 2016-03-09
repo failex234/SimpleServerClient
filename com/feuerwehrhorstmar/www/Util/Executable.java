6974265b6ea74e38c5e16a9faf29fe78dea836ca
Leonard <DeBukkIt@users.noreply.github.com>
2016-03-09 12:43:59 +0100

    Create Executable.java

---
+++ com/feuerwehrhorstmar/www/Util/Executable.java
@@ -0,0 +1,16 @@
+package com.feuerwehrhorstmar.www.Util;
+
+import java.net.Socket;
+
+public interface Executable {
+	
+	/**
+	 * Put the lines to be executed inside the run.<br>
+	 * This method is called by the Client or Server if the identifier<br>
+	 * of an Datapackage fits to the identifier this Executable is registered with.
+	 * @param msg the Datapackage to work with
+	 * @param socket the Socket to work with
+	 */
+	public abstract void run(Datapackage msg, Socket socket);
+
+}
