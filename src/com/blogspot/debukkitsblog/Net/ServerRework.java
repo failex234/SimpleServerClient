69fcb6395cd4064fbadb442d1dce67a7c5759e96
failex234 <felix@emilnaumann.de>
2017-06-18 21:13:04 +0200

    Rework almost done

--- src/com/blogspot/debukkitsblog/Net/ServerRework.java
+++ src/com/blogspot/debukkitsblog/Net/ServerRework.java
@@ -1,14 +1,22 @@
 package com.blogspot.debukkitsblog.Net;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Created by felix on 18.06.17.
+ * A simple rework of the original SimpleServerClient
+ * project by Leonard Bienbeck (DeBukkit).
+ * This code is essentially very similar to the original one
+ * but still a bit different
+ *
+ * @author Felix Naumann
+ * @version 0.0.1
  */
 public abstract class ServerRework {
 
@@ -23,6 +31,13 @@ public abstract class ServerRework {
 
     private Thread connectionListenerThread;
 
+    /**
+     * Constructor for the server. The timeout parameter currently does
+     * nothing.
+     *
+     * @param port    The port the server should listen on
+     * @param timeout The time in seconds when a non-responding client should be unregistered-
+     */
     public ServerRework(int port, int timeout) {
         this.port = port;
         this.timeout = timeout;
@@ -35,19 +50,15 @@ public abstract class ServerRework {
     public abstract void preStart();
 
     public void onClientConnected(Socket client) {
-
     }
 
     public void onClientRegistered(Socket client, String name) {
-
     }
 
     public void onClientUnregistered(Socket client, String name) {
-
     }
 
     public void onClientDisconnected(Socket client) {
-
     }
 
 
@@ -107,6 +118,12 @@ public abstract class ServerRework {
         connectionListenerThread.start();
     }
 
+    /**
+     * Registers a client
+     *
+     * @param client The Client to register
+     * @param name   The nickname of the client for better identification
+     */
     private void registerClient(Socket client, String name) {
         if (!isRegistered(client)) {
             clients.add(client);
@@ -114,6 +131,11 @@ public abstract class ServerRework {
         }
     }
 
+    /**
+     * Unregister a client
+     *
+     * @param client The client to unregister
+     */
     private void unregisterClient(Socket client) {
         if (isRegistered(client)) {
             clients.remove(client);
@@ -121,12 +143,24 @@ public abstract class ServerRework {
         }
     }
 
+    /**
+     * Change name of a client. If the client wants to change his nickname
+     *
+     * @param client The client to change the nickname of
+     * @param name   New nickname for the client
+     */
     private void setName(Socket client, String name) {
         if (isRegistered(client)) {
             clientnames.replace(client, name);
         }
     }
 
+    /**
+     * Get name of a client
+     *
+     * @param client The client to get the name from
+     * @return The name of the given client, null if client is not registered
+     */
     private String getName(Socket client) {
         if (isRegistered(client)) {
             return clientnames.get(client);
@@ -134,6 +168,12 @@ public abstract class ServerRework {
         return null;
     }
 
+    /**
+     * Check if a client is registered / connected
+     *
+     * @param client The client to check
+     * @return true if client is connected / registered, otherwise false
+     */
     private boolean isRegistered(Socket client) {
         if (clients.contains(client)) return true;
         return false;
@@ -144,6 +184,11 @@ public abstract class ServerRework {
         return false;
     }
 
+    /**
+     * Registration of some standard methods that should exist from
+     * the beginning to react to and with some basic i/o to and from
+     * clients.
+     */
     private void registerStandardMethods() {
         /**
          * Only register client when nickname is given.
@@ -234,20 +279,41 @@ public abstract class ServerRework {
         });
 
         methods.put("_INTERNAL_LOGOUT_", (pack, socket) -> {
-                onClientUnregistered(socket, getName(socket));
-                unregisterClient(socket);
-                log("Client \"" + socket.getInetAddress() + "\"(" + getName(socket) + ") just unregistered");
-                log("Sending status...");
-                sendPackage(new Datapackage("STATUS", "OK"), socket);
-                onClientDisconnected(socket);
+            onClientUnregistered(socket, getName(socket));
+            unregisterClient(socket);
+            log("Client \"" + socket.getInetAddress() + "\"(" + getName(socket) + ") just unregistered");
+            log("Sending status...");
+            sendPackage(new Datapackage("STATUS", "OK"), socket);
+            onClientDisconnected(socket);
         });
 
     }
 
+    /**
+     * Sends a Datapackage to a client
+     *
+     * @param content  The Datapackage to sent (First element should always be the identifier!)
+     * @param receiver The client to send the Datapackage to
+     */
     public void sendPackage(Datapackage content, Socket receiver) {
-        //TODO
+        if (receiver.isConnected()) {
+            try {
+                ObjectOutputStream out = new ObjectOutputStream(receiver.getOutputStream());
+                out.writeObject(content);
+            } catch (IOException e) {
+                log("An Exception occurred whilst trying to send a package to \"" + receiver.getInetAddress().toString() + "\"(" + getName(receiver) + ")");
+                e.printStackTrace();
+            }
+        } else {
+            log("Socket not connected!");
+        }
     }
 
+    /**
+     * Broadcasts a Datapackage to all connected clients
+     *
+     * @param content The Datapackage to broadcast
+     */
     public void broadcast(Datapackage content) {
         log("Broadcasting to all clients...");
         for (int i = 0; i < clients.size(); i++) {
@@ -255,6 +321,98 @@ public abstract class ServerRework {
         }
     }
 
+    /**
+     * Register any method except for the already standard ones
+     *
+     * @param identifier The identifier of the method
+     * @param e          The Executable (The code that gets executed)
+     */
+    public void registerMethod(String identifier, Executable e) {
+        if (identifier.equals("_INTERNAL_LOGIN_")) {
+            throw new IllegalArgumentException("Identifier may not be '_INTERNAL_LOGIN_'. "
+                    + "This method is already taken for communication between "
+                    + "the server and the respective clients.");
+        } else if (identifier.equals("_MSG_")) {
+            throw new IllegalArgumentException("Identifier may not be '_MSG_'. "
+                    + "This method is already taken for communication between "
+                    + "the server and the respective clients.");
+        } else if (identifier.equals("_BROADCAST_")) {
+            throw new IllegalArgumentException("Identifier may not be '_BROADCAST_'. "
+                    + "This method is already taken for communication between "
+                    + "the server and the respective clients.");
+        } else if (identifier.equals("_GET_NAME_")) {
+            throw new IllegalArgumentException("Identifier may not be '_GET_NAME_'. "
+                    + "This method is already taken for nickname exchange "
+                    + "between the server and the respective clients");
+        } else if (identifier.equals("_SET_NAME_")) {
+            throw new IllegalArgumentException("Identifier may not be '_SET_NAME_'. "
+                    + "This method is already taken for nickname exchange "
+                    + "between the server and the respective clients");
+        } else if (methods.containsKey(identifier)) {
+            throw new KeyAlreadyExistsException("Identifier may not be '" + identifier + "'."
+                    + "This method is already registered. If you want to "
+                    + "replace the method you have to use unregisterMethod() first!");
+        } else {
+            methods.put(identifier, e);
+        }
+    }
+
+    /**
+     * Unregisters a method
+     *
+     * @param identifier The method to unregister
+     */
+    public void unregisterMethod(String identifier) {
+        if (identifier.equalsIgnoreCase("_INTERNAL_LOGIN_")) {
+            throw new IllegalArgumentException("Unable to unregister '_INTERNAL_LOGIN_'. " +
+                    "This method cannot be unregistered!");
+        } else if (identifier.equalsIgnoreCase("_MSG_")) {
+            throw new IllegalArgumentException("Unable to unregister '_MSG_'. " +
+                    "This method cannot be unregistered!");
+        } else if (identifier.equalsIgnoreCase("_BROADCAST_")) {
+            throw new IllegalArgumentException("Unable to unregister '_BROADCAST_'. " +
+                    "This method cannot be unregistered!");
+        } else if (identifier.equalsIgnoreCase("_GET_NAME_")) {
+            throw new IllegalArgumentException("Unable to unregister '_GET_NAME_'. " +
+                    "This method cannot be unregistered!");
+        } else if (identifier.equalsIgnoreCase("_SET_NAME_")) {
+            throw new IllegalArgumentException("Unable to unregister '_SET_NAME_'. " +
+                    "This method cannot be unregistered!");
+        } else if (!methods.containsKey(identifier)) {
+            throw new IllegalArgumentException("Unable to unregister '" + identifier + "'. " +
+                    "This method is not registered!");
+        } else {
+            methods.remove(identifier);
+        }
+    }
+
+    /**
+     * Returns a count of all connected client
+     *
+     * @return The count of all connected / registered clients
+     */
+    public int getClientCount() {
+        return this.clients.size();
+    }
+
+    /**
+     * Kill the server
+     */
+    public void kill() {
+        try {
+            connectionListenerThread.interrupt();
+            server.close();
+        } catch (Exception e) {
+            log("An error occurred whilst trying to kill the server...");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Log to console
+     *
+     * @param msg The message to log
+     */
     private void log(String msg) {
         System.out.println("[SERVER] " + msg);
     }
